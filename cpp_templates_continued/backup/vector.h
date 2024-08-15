75b2bce9e7877f421f14d46c523af0929bfe8231
TheTepidCoder <jacob.a.bethany@gmail.com>
2024-08-15 02:37:46 -0400

    TheTepidCoder: I've added the code from the video that I published on 2024-08-15.

---
+++ cpp_templates_continued/backup/vector.h
@@ -0,0 +1,55 @@
+/*
+ Created on 2024-08-04 by TheTepidCoder
+ Purpose: To show how to define C++ template classes,
+ rather than just templated functions.
+*/
+#pragma once
+#ifndef VECTOR_HEADER
+#define VECTOR_HEADER
+
+#include "stdio.h"
+#include "stdlib.h"
+#include "stdint.h"
+#include "inttypes.h"
+
+#ifndef DEBUG_PRINTF_MACRO
+#define DEBUG_PRINTF_MACRO
+#define debug_printf(...) \
+  fprintf ( \
+    stdout, \
+    "[%s:%" PRIu64 "] ", \
+    __FILE__, \
+    static_cast<uint64_t> ( __LINE__ ) \
+  ); \
+  fprintf ( \
+    stdout, \
+    __VA_ARGS__ \
+  ); \
+  fflush ( stdout )
+#endif
+
+template<typename T>
+class vector {
+  private:
+    T *m_lpt_data = nullptr;
+    size_t m_st_count = 0; //number of elements currently stored
+    size_t m_st_capacity = 0; //number of elements that could be stored
+
+  public:
+    vector (  );
+    ~vector (  );
+    //bool push_back ( const T t );
+    bool push_back ( const T &t );
+    //void move_back ( T &&t ); //std::move ( t )
+
+    const T *begin ( void ) const; //for ( const auto &v : vector_instance ) { ... }
+    const T *end ( void ) const;
+    T *begin ( void );
+    T *end ( void );
+
+    const T *data ( void ) const;
+    T *data ( void );
+    size_t count ( void ) const;
+};
+
+#endif
